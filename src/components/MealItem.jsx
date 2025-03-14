3a0e65cce1b0bd368c8b74170e657d82333db82c
Monica Antao <monica.sn.antao@gmail.com>
2025-03-14 11:31:55 +0000

    apply new Button component

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,16 +1,22 @@
 import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
 
 export default function MealItem({ mealData }) {
   return (
     <li className="meal-item">
       <article>
-        <img src={`http://localhost:3000/${mealData.image}`} alt={mealData.name} />
+        <img
+          src={`http://localhost:3000/${mealData.image}`}
+          alt={mealData.name}
+        />
         <div>
           <h3>{mealData.name}</h3>
-          <p className="meal-item-price">{currencyFormatter.format(mealData.price)}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(mealData.price)}
+          </p>
           <p className="meal-item-description">{mealData.description}</p>
           <p>
-            <button className="meal-item-actions">Add to cart</button>
+            <Button className="meal-item-actions">Add to cart</Button>
           </p>
         </div>
       </article>
