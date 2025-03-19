ef1e5ed70b7d490f2404bc7db4b731759240085b
Monica Antao <monica.sn.antao@gmail.com>
2025-03-19 14:50:38 +0000

    create a post requet to submit data/orders

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -19,12 +19,35 @@ export default function Checkout() {
     userProgressCtx.hideCart();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const consumerData = Object.fromEntries(fd.entries());
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: consumerData,
+        },
+      }),
+    });
+  }
+
   return (
-    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleCloseCheckout}>
-      <form>
+    <Modal
+      open={userProgressCtx.progress === "checkout"}
+      onClose={handleCloseCheckout}
+    >
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full name" type="text" id="full-name" />
+        <Input label="Full name" type="text" id="name" />
         <Input label="E-mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
